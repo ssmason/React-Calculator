36592c6d4d27db973bf9bb5f7b4ae3b55f2c3173
stephen mason <stephenmason@stephens-MBP.OleLan>
2025-08-25 12:54:46 +0200

    chore: add in a calculator.html page for render on a html page

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
+import './index.scss';
 import App from "./App.jsx";
+import { Calculator } from "./components/Calculator.jsx";
 import reportWebVitals from './reportWebVitals.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// Look for either #root (App) or #calculator (Calculator)
+const appRoot = document.getElementById("root");
+const calcRoot = document.getElementById("calculator");
+
+if (appRoot) {
+  ReactDOM.createRoot(appRoot).render(
     <React.StrictMode>
-        <App />
+      <App />
     </React.StrictMode>
-);
+  );
+}
+
+if (calcRoot) {
+  ReactDOM.createRoot(calcRoot).render(
+    <React.StrictMode>
+      <Calculator />
+    </React.StrictMode>
+  );
+}
 
 reportWebVitals();
